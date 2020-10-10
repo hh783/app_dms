c8a7688e363317d4f11b8d621e7c9a1c0621c707
hh783 <63758059+hh783@users.noreply.github.com>
2020-10-10 12:11:21 -0600

    Add files via upload

--- dms_app-1.0.0/js/js_main.js
+++ dms_app-1.0.0/js/js_main.js
@@ -2619,7 +2619,7 @@ function setPDVFordis(vIdPDV){
                             vMontoEstimado = parseInt(results.rows[0].sat) ;
                             break;            
                         } // fin del switch 
-                        alert("* PDV "+ vIdPdvG+" *\nVenta Promedio Semanal Epin:\n L. " + vMontoSugerido + "\nVenta Sugerida Diaria Epin:\n L. " + vMontoEstimado + "\n");
+                        alert("* PDV "+ vIdPdvG+" *\nVenta Epin Promedio Semanal:\n L. " + vMontoSugerido + "\nVenta Epin Sugerida del día:\n L. " + vMontoEstimado + "\n");
                     }                  
                     /*
                     for(var i=0;i<len; i++){
